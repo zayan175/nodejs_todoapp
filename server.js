bb4c2906d667dcf62304b2874c1f98e07e8c09a8
zayan175 <124209692+zayan175@users.noreply.github.com>
2024-01-08 17:07:07 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const app = require('./app.js');
 const connectDB = require('./data/database.js');
+const PORT = process.env.PORT || 5000
 
 connectDB();
 
 
-app.listen(process.env.PORT,(req,resp)=>{
+app.listen(PORT,(req,resp)=>{
     
     console.log(`Server is Working on port : ${process.env.PORT} in ${process.env.NODE_ENV } Mode`)
 })
